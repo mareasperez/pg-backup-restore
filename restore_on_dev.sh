c4683ab2e8b5bba0a95a6cad7f2fe458c1cf8ca2
Maury Areas <mareasperez7@gmail.com>
2022-01-17 16:18:38 -0600

    feat:  confirm backup on restore

--- restore_on_dev.sh
+++ restore_on_dev.sh
@@ -3,41 +3,51 @@
 ####### LOAD CONFIG #######
 ###########################
 function load_config() {
-if [ -z $CONFIG_FILE_PATH ] ; then
-        SCRIPTPATH=$(cd ${0%/*} && pwd -P)
-        CONFIG_FILE_PATH="${SCRIPTPATH}/${config}"
-fi
+        if [ -z $CONFIG_FILE_PATH ]; then
+                SCRIPTPATH=$(cd ${0%/*} && pwd -P)
+                CONFIG_FILE_PATH="${SCRIPTPATH}/${config}"
+        fi
 
-if [ ! -r ${CONFIG_FILE_PATH} ] ; then
-        echo "Could not load config file from ${CONFIG_FILE_PATH}" 1>&2
-        exit 1
-fi
-source "${CONFIG_FILE_PATH}"
+        if [ ! -r ${CONFIG_FILE_PATH} ]; then
+                echo "Could not load config file from ${CONFIG_FILE_PATH}" 1>&2
+                exit 1
+        fi
+        source "${CONFIG_FILE_PATH}"
 }
 function load_dev_config() {
-    echo "Backup dev"
-    config="dev.env"
-    folder_name="dev"
+        echo "Backup dev"
+        config="dev.env"
+        folder_name="dev"
 }
 
 function backup_db() {
-export PGPASSWORD=$DB_PASSWORD
-now=$(date)
-echo "Starting backup script: $now"
-echo "Connecting to database: $DB_DATABASE"
-# echo "pg_dump -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE >${SCRIPTPATH}/backups/${folder_name}/${folder_name}-$now.sql"
-pg_dump -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE > "${SCRIPTPATH}/backups/${folder_name}/${folder_name}-$now.sql"
-echo "Backup done: $(date)"
+        export PGPASSWORD=$DB_PASSWORD
+        now=$(date)
+        echo "Starting backup script: $now"
+        echo "Connecting to database: $DB_DATABASE"
+        # echo "pg_dump -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE >${SCRIPTPATH}/backups/${folder_name}/${folder_name}-$now.sql"
+        pg_dump -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE >"${SCRIPTPATH}/backups/${folder_name}/${folder_name}-$now.sql"
+        echo "Backup done: $(date)"
 }
 
 function restore_db() {
-export PGPASSWORD=$DB_PASSWORD
-now=$(date)
-echo "Starting restore script: $now"
-echo "Connecting to database: $DB_DATABASE"
-echo "psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE -f "${SCRIPTPATH}/transfer.sql""
-psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE -f"${SCRIPTPATH}/transfer.sql"
-echo "restore terminado"
+        export PGPASSWORD=$DB_PASSWORD
+        now=$(date)
+        echo "Starting restore script: $now"
+        echo "Connecting to database: $DB_DATABASE"
+        echo "psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE -f "${SCRIPTPATH}/transfer.sql""
+        psql -U $DB_USERNAME -h $DB_HOST -p $DB_PORT $DB_DATABASE -f"${SCRIPTPATH}/transfer.sql"
+        echo "restore terminado"
+}
+function confim_backup() {
+        echo "Confirma backup? (y/n)"
+        read confirma
+        if [ "$confirma" = "y" ]; then
+                echo "Backup confirmado"
+                backup_db
+        else
+                echo "Backup cancelado"
+        fi
 }
 SECONDS=0
 config=""
@@ -45,7 +55,7 @@ folder_name=""
 SCRIPTPATH=""
 load_dev_config
 load_config
-backup_db
+confim_backup
 restore_db
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
