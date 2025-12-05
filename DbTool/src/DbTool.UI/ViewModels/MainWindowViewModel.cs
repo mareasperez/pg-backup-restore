a9890d92eebd47a0eb80856eae066a452c10e664
Mauricio.Areas <mareasperez7@gmail.com>
2025-12-05 09:02:09 -0600

    feat: Add initial Avalonia UI project for DbTool.

---
+++ DbTool/src/DbTool.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace DbTool.UI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ DbTool/src/DbTool.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DbTool.UI.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
